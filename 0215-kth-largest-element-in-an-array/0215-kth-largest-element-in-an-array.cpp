813be3ea8ae330a5eea483db829b92336201d76a
Manjari Saxena <manjari.25mcan0186@jecrcu.edu.in>
2026-06-27 15:16:41 +0530

    Time: 38 ms (40.17%), Space: 61.7 MB (59.84%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+
+        priority_queue<int, vector<int>, greater<int>> minheap;
+
+        for(int num : nums) {
+
+            minheap.push(num);
+
+            // heap size k se zyada ho toh smallest remove
+            if(minheap.size() > k) {
+                minheap.pop();
+            }
+        }
+
+        return minheap.top();
+    }
+};
