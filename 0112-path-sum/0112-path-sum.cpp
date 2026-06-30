f0d24238ef6e28c2f40143a2a37087fd259bc44b
Manjari Saxena <manjari.25mcan0186@jecrcu.edu.in>
2026-06-30 15:14:16 +0530

    Time: 0 ms (100%), Space: 21.4 MB (66.56%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr)
+            return false;
+
+        targetSum -= root->val;
+
+        // Leaf node
+        if (root->left == nullptr && root->right == nullptr)
+            return targetSum == 0;
+
+        return hasPathSum(root->left, targetSum) ||
+               hasPathSum(root->right, targetSum);
+    }
+};
